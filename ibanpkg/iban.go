f7413d2301e7ba414a7a3c4a9c3a6a2855e93da9
knoxgon <volkan.guven@hotmail.se>
2019-06-26 19:57:49 +0200

    Added comments, descriptions and references

--- ibanpkg/iban.go
+++ ibanpkg/iban.go
@@ -1,9 +1,34 @@
 package ibanpkg
 
 /*
+	Author: Volkan Güven
+	github: https://github.com/knoxgon/
+
 	Resources:
 	https://usersite.datalab.eu/printclass.aspx?type=wiki&id=91772
 	https://bank.codes/iban/structure
+	https://en.wikipedia.org/wiki/International_Bank_Account_Number
+
+	Wikipedia Quote:
+
+	Validating the IBAN
+	An IBAN is validated by converting it into an integer and performing a basic mod-97 operation
+	(as described in ISO 7064) on it. If the IBAN is valid, the remainder equals 1.
+	The algorithm of IBAN validation is as follows:
+
+	  Check that the total IBAN length is correct as per the country. If not, the IBAN is invalid
+	 Move the four initial characters to the end of the string
+	 Replace each letter in the string with two digits, thereby expanding the string, where A = 10, B = 11, ..., Z = 35
+	 Interpret the string as a decimal integer and compute the remainder of that number on division by 97
+	 If the remainder is 1, the check digit test is passed and the IBAN might be valid.
+
+	Example (fictitious United Kingdom bank, sort code 12-34-56, account number 98765432):
+
+	• IBAN:		GB82 WEST 1234 5698 7654 32
+	• Rearrange:		W E S T12345698765432 G B82
+	• Convert to integer:		3214282912345698765432161182
+	• Compute remainder:		3214282912345698765432161182	mod 97 = 1
+
 */
 
 import (
@@ -12,6 +37,8 @@ import (
 	"strings"
 )
 
+//getIbanCodes() returns a IBAN map that has the country code and corresponding length
+//I.e., Sweden has iso2 SE and iban number length 22
 func getIbanCodes() map[string]int {
 	return map[string]int{
 		"AL": 28,
@@ -94,37 +121,40 @@ func getIbanCodes() map[string]int {
 	}
 }
 
+//controlIso2AndLength controls the first 2 iban characters
+//to match it with the existing iban conditions from getIbanCodes()
 func controlIso2AndLength(iban string) bool {
 	//Trim spaces
 	iban = strings.Replace(iban, " ", "", -1)
 
-	//If iso2 exists in the library and total length is matched
+	//If iso2 exists in the library and total length is matched and minimum length is 5
 	for k, v := range getIbanCodes() {
-		if k == iban[0:2] && len(iban) == v {
+		if k == iban[0:2] && len(iban) == v && len(iban) >= 5 {
 			return true
 		}
 	}
 	return false
 }
 
+//charToIso7064 Converts each alphanumeric characters from A to Z to base 36.
+//Numbers 0 to 9 are converted to ASCII so i.e., '5' is 5
 func charToIso7064(char rune) int64 {
+	digit := int64(char)
 	if char >= 'A' && char <= 'Z' {
-		digita := int64(char)
-
-		if digita > 64 && digita < 91 {
-			digita -= 55
-			return digita
+		if digit > 64 && digit < 91 {
+			digit -= 55
+			return digit
 		}
 	} else if char >= '0' && char <= '9' {
-		digit := int64(char)
 		digit -= '0'
 		return digit
 	}
 	return -1
 }
 
-/*ControlIban function controls the iban number
- */
+/*ControlIban controls the iban number
+The return type is a condition whether the given iban is valid or not.
+*/
 func ControlIban(iban string) bool {
 	iban = strings.ToUpper(strings.Replace(iban, " ", "", -1))
 
@@ -132,29 +162,43 @@ func ControlIban(iban string) bool {
 		return false
 	}
 
+	//Slice the countrycode and checksum
 	countryCode := iban[0:4]
+	//Get the rest of the bban
 	bban := iban[4:]
 
+	//Move the iso2 and checksum to the end of the string
 	rotatediban := bban + countryCode
 
+	//New string with the standard iso7064 is being written to rearrange
 	rearrange := ""
 
+	//The for loop converts every character to base36,
+	//any character not meeting the requirement breaks the function
+	//(Probable faulty IBAN)
 	for _, c := range rotatediban {
 		elem := charToIso7064(c)
-		if elem != -1 {
-			rearrange += strconv.Itoa(int(elem))
+		if elem == -1 {
+			return false
 		}
+		rearrange += strconv.Itoa(int(elem))
 	}
 
+	//After converting the iban to base 36, write it back as a string
+	//Since the number is too large, uint64 cannot hold it, thus SetString
+	//is used to hold larger numbers
 	ibanAsInt, isOk := new(big.Int).SetString(rearrange, 10)
 
 	if !isOk {
-		panic("Error happened")
+		panic("Error happened while processing 'rearrange'")
 	}
 
+	//Since big.Int.Mod(x,y) function returns *Int, we have to have modulus 97 as a big int
 	mod := big.NewInt(97)
 
+	//The result is either 1 or non-1, anything else beside 1 is false
 	result := new(big.Int).Mod(ibanAsInt, mod)
 
+	//result is a pointer to an Int, so, converting to Int64() allows us to make comparision
 	return result.Int64() == 1
 }
